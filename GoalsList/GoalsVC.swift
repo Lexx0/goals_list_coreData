ad067468d9c45e15b3d97632766e5843c7230210
YOUR NAME <zoid@macbook-pro-aberezovsky.local>
2017-09-20 18:54:46 +0300

    VC

---
+++ GoalsList/GoalsVC.swift
@@ -0,0 +1,28 @@
+//
+//  VGoalsVC.swift
+//  GoalsList
+//
+//  Created by Alex Berezovsky on 20.09.17.
+//  Copyright © 2017 PatataMatata. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tablewView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func goalBtnPressed(_ sender: Any) {
+        
+        
+    }
+}
+
