4effbc215658b8016258dc6678d14e8a9a72f46d
YOUR NAME <zoid@macbook-pro-aberezovsky.local>
2017-09-20 20:50:11 +0300

    enums done

---
+++ GoalsList/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalsList
+//
+//  Created by Alex Berezovsky on 20.09.17.
+//  Copyright © 2017 PatataMatata. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm  = "Short Term"
+}
--- GoalsList/GoalsVC.swift
+++ GoalsList/GoalsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
@@ -17,6 +18,10 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tablewView.delegate = self
+        tablewView.dataSource = self
+        tablewView.isHidden = false
 
     }
 
@@ -26,3 +31,25 @@ class GoalsVC: UIViewController {
     }
 }
 
+
+extension GoalsVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tablewView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell()}
+        
+        cell.configureCell(description: "Eat shit and fart", type: .shortTerm, goalProgressAmount: 2)
+//        cell.configureCell(description: "Eat shit and fart", type: "short term", goalProgressAmount: 2)
+        
+        return cell
+        
+    }
+}
