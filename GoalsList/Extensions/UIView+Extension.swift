44b7ce4c340f5a07d2bb124f8e31ac978c476ec9
YOUR NAME <zoid@macbook-pro-aberezovsky.local>
2017-09-21 14:47:40 +0300

    UIAnimation extension for TextView

---
+++ GoalsList/Extensions/UIView+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIView+Extension.swift
+//  GoalsList
+//
+//  Created by Alex Berezovsky on 21.09.17.
+//  Copyright © 2017 PatataMatata. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func bindToKeyBoard() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBordWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+    }
+    
+    func keyBordWillChange(_ notification: NSNotification) {
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let startingFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endingFrame.origin.y - startingFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+        
+    }
+}
